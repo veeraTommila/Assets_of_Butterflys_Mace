abf302dca6a4ad1c0cfd4f4747b0c01516f8580b
Veera Tommila <veera.tommila@gmail.com>
2024-09-18 12:44:48 +0300

    Update AudioSlider.cs
    Changed the inclusion of the whole script.

--- Scripts/AudioSlider.cs
+++ Scripts/AudioSlider.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioSlider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*
+     * This creates a private field for the slider element, makes it visible in the Unity Editor,
+     * and allows one to assign the slider trhough the Inspector.
+     */
+    [SerializeField] private Slider volumeSlider;
+    
+    // A private method called when the script instance is loaded.
+    private void Start()
     {
-        
+        // To set the slider's value to the saved volume level from PlayerPrefs, or to 1 if no value is found.
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f);
+
+        // To make sure that the global volume level corresponds the slider's value.
+        AudioListener.volume = volumeSlider.value;
     }
 
-    // Update is called once per frame
-    void Update()
+    // a public method to change the volume.
+    public void ChangeVolume(float volume)
     {
-        
+        // To set the global volume level to the specified value.
+        AudioListener.volume = volume;
+
+        // To save the specified volume level to PlayerPrefs for later retrieveing.
+        PlayerPrefs.SetFloat("Volume", volume);
     }
 }
