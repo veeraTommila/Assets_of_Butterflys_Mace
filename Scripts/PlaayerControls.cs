0770c4a257b7fa544f36702212a2288ecc29b9c5
Veera Tommila <veera.tommila@gmail.com>
2025-09-21 18:04:38 +0300

    Update PlaayerControls.cs
    Removed both duplicate declarations and unused or commented-out code. Simplified gravity application. Adjusted damage handling. Initialized jump force. - Removed parentheses around Mathf.Abs(...) + Mathf.Abs(...)
    to make the code look like cleaner. Modified the test damage block under Update() to apply 0.5f damage instead of 1f, and commented it out — likely for debugging or tuning. Removed redundant assignment. Fixed formatting and indentation

--- Scripts/PlaayerControls.cs
+++ Scripts/PlaayerControls.cs
@@ -5,15 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class PlaayerControls : MonoBehaviour
 {
-    public float moveSpeed;   //Tätä muuttujaa moveSpeed voidaan editoida Unityssä, private floatia ei voida.
-    private Rigidbody theRB;   //Otetaan Rigidbody käyttöön.
-    private float jumpForce;
+    public float moveSpeed;
+    private Rigidbody theRB;
+    public float jumpForce = 5f;
     public CharacterController controller;
 
     private Vector3 moveDirection;
     public float gravityScale;
     private Vector3 originalPosition;
-    private Vector3 currentPosition;
 
     public Animator CharacterAnimator;
     public Transform pivot;
@@ -27,28 +26,25 @@ public class PlaayerControls : MonoBehaviour
     public float maximumHealth = 3;
     public float currentHealth;
     public HealthBar healthBar;
-    Collider other;
 
     private Camera mainCam;
     private float maxRayDistance = 10f;
     private int floorMask;
-    private Transform turret;
-    private float turretTurningSpeed;
-    private Transform muzzle;
-    //public GameObject projectile;
-    private float time;
-    private float shootingCooldownTime;
+    public Transform turret;
+    public float turretTurningSpeed;
 
     public GameObject RestartMenu;
 
     public AudioClip damageSound;
-    public AudioSource deathSound;
+    public AudioClip deathSound;
 
-    // Start is called before the first frame update
+    private HashSet<GameObject> alreadyHitProjectiles = new HashSet<GameObject>();
+
+
     void Start()
     {
         PlayerDie = false;
-        Enemy = GameObject.FindWithTag("Muzzle"); // Sets the variable to the GameObject that has a "Projectile" tag.       
+        Enemy = GameObject.FindWithTag("Muzzle");
 
         controller = GetComponent<CharacterController>();
         theRB = GetComponent<Rigidbody>();
@@ -59,37 +55,28 @@ public class PlaayerControls : MonoBehaviour
 
         mainCam = Camera.main;
         floorMask = LayerMask.GetMask("Floor");
-        // A sanity check to make sure the player is not dead.
         CharacterAnimator.SetBool("PlayerDies", false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //Pelinappulan liikkeen kontrollointi on helpompaa.
         float yStore = moveDirection.y;
         moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
         moveDirection = moveDirection.normalized * moveSpeed;
         moveDirection.y = yStore;
 
-
-        // Has the jump key been pressed in Unity?
         if (controller.isGrounded)
         {
             moveDirection.y = 0f;
             if (Input.GetButtonDown("Jump"))
             {
-                // Let's change the value of the Y-axis.
                 moveDirection.y = jumpForce;
             }
         }
 
-        moveDirection.y = moveDirection.y + (Physics.gravity.y * Time.deltaTime * gravityScale);
-
-        // Apply moveDirection to Character Controller.
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
         controller.Move(moveDirection * Time.deltaTime);
 
-        // The game character rotates according to the direction the camera is looking.
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
             transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
@@ -98,13 +85,13 @@ public class PlaayerControls : MonoBehaviour
         }
 
         CharacterAnimator.SetBool("isGrounded", controller.isGrounded);
-        CharacterAnimator.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
-
+        CharacterAnimator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal")));
+/*
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            TakeDamage(1);
-            AudioSource.PlayClipAtPoint(damageSound, transform.position, 1f);
-        }
+            TakeDamage(0.5f);
+            AudioSource.PlayClipAtPoint(damageSound, transform.position, 0.5f);
+        }*/
     }
 
     void FixedUpdate()
@@ -112,19 +99,14 @@ public class PlaayerControls : MonoBehaviour
         Vector3 pos = transform.position;
         pos.y = 0;
         transform.position = pos;
-        //theRB.MovePosition(pos);
-        //theRB.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
 
-        // The movement of the player object will be easier.
         float yStore = moveDirection.y;
         moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
         moveDirection = moveDirection.normalized * moveSpeed;
         moveDirection.y = yStore;
 
-        // Let us apply the moveDirection to controller.
         controller.Move(moveDirection * Time.deltaTime);
 
-        // The player object turns to the camera's watching angle.
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
             transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
@@ -133,13 +115,10 @@ public class PlaayerControls : MonoBehaviour
         }
 
         CharacterAnimator.SetBool("isGrounded", controller.isGrounded);
-        //CharacterAnimator.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
 
-        //The ray.
         Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        
-        //If the raycast with ray hit's something, this happens with max distance.
+
         if (Physics.Raycast(ray, out hit, maxRayDistance))
         {
             Vector3 targetDirection = hit.point - turret.position;
@@ -147,53 +126,61 @@ public class PlaayerControls : MonoBehaviour
             Vector3 turningDirection = Vector3.RotateTowards(turret.forward, targetDirection, turretTurningSpeed * Time.deltaTime, 0f);
             turret.rotation = Quaternion.LookRotation(turningDirection);
         }
-    } // End of FixedUpdate function.
-
-    
+    }
 
     public void TakeDamage(float damage)
     {
-        AudioSource.PlayClipAtPoint(damageSound, transform.position, 1f);
+        AudioSource.PlayClipAtPoint(damageSound, transform.position, 0.5f);
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
 
         if (currentHealth <= 0)
         {
             currentHealth = 0;
-            //Character dies.
-            //The death animation is played.
             CharacterAnimator.SetBool("PlayerDies", true);
-            // The player won't move.
-            GetComponent<PlayerController>().enabled = false;
+            GetComponent<PlaayerControls>().enabled = false;
             Debug.Log("Player is dead.");
-            //AudioSource.PlayClipAtPoint(deathSound, transform.position, 1f);
-            //userInterface.ShowRestartMenu();
+            AudioSource.PlayClipAtPoint(deathSound, transform.position, 0.5f);
             PlayerDie = true;
         }
 
-        if (PlayerDie == true)
+        if (PlayerDie)
         {
-            // Play Game Over on screen.
             RestartMenu.gameObject.SetActive(true);
-            Debug.Log("YOU DIED! This will tell that the game over screen will be played now.");
-            //deathSound.Play();
-            // Reload the whole scene and reset all the gameobjects.
-            //Scene currentScene = SceneManager.GetActiveScene();
-            //SceneManager.LoadScene(currentScene.name);
-            // To set the respawn place of the player.
+            Debug.Log("YOU DIED! Game over screen activated.");
             Player.transform.position = respawnpoint.position;
         }
-        //RestartMenu.gameObject.SetActive(false);
-
     }
 
     public void Heal(float damage)
     {
         currentHealth += damage;
-
         if (currentHealth > maximumHealth)
         {
             currentHealth = maximumHealth;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Muzzle") && !alreadyHitProjectiles.Contains(other.gameObject))
+        {
+            alreadyHitProjectiles.Add(other.gameObject);
+            TakeDamage(0.5f);
+            AudioSource.PlayClipAtPoint(damageSound, transform.position, 0.5f);
+
+            // Optional: destroy the projectile after impact
+            Destroy(other.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Muzzle"))
+        {
+            TakeDamage(0.5f);
+            AudioSource.PlayClipAtPoint(damageSound, transform.position, 0.5f);
+        }
+    }
+
 }
